afb2c2ff128bf5feead837eb3004c256c778ac05
jmtatsch <jmtatsch@users.noreply.github.com>
2019-10-17 12:37:01 +0200

    Update compile.sh
    fix non existing udev flag

--- compile.sh
+++ compile.sh
@@ -1,13 +1,16 @@
+#!/usr/bin/env bash
+
 TOOLCHAIN=$(pwd)/../toolchain/bin
 CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
-echo "Using the following Toolchain $TOOLCHAIN"
-make clean
+
+echo "Using Toolchain $TOOLCHAIN"
+
 export CC=${CROSS_COMPILE}gcc
 export CFLAGS="-muclibc -O2"
 export CPPFLAGS="-muclibc -O2"
 export LDFLAGS="-muclibc -O2"
 
-./configure --host=mips-linux-gnu --disable-udev --prefix=${PWD}/_install
+make clean
+./configure --host=mips-linux-gnu --prefix=${PWD}/_install
 make libmp3lame.so
 make install
-
